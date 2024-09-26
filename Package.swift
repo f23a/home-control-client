1d7375f2d5ec4af61d3633422b3e70946d6ff2ef
Christoph Pageler <christoph.pageler@me.com>
2024-09-26 20:38:23 +0200

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,32 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "home-control-client",
+    platforms: [
+        .iOS(.v17),
+        .macOS(.v14),
+        .watchOS(.v10),
+        .tvOS(.v17)
+    ],
+    products: [
+        .library(name: "HomeControlClient", targets: ["HomeControlClient"])
+    ],
+    dependencies: [
+        .package(path: "../home-control-kit")
+    ],
+    targets: [
+        .target(
+            name: "HomeControlClient",
+            dependencies: [
+                .product(name: "HomeControlKit", package: "home-control-kit")
+            ]
+        ),
+        .testTarget(
+            name: "HomeControlClientTests",
+            dependencies: ["HomeControlClient"]
+        )
+    ]
+)
---
+++ Sources/HomeControlClient/Routes/InverterReadingRoutes.swift
@@ -0,0 +1,21 @@
+//
+//  InverterReadingRoutes.swift
+//  HomeControlKit
+//
+//  Created by Christoph Pageler on 30.04.24.
+//
+
+import Foundation
+import HomeControlKit
+
+public struct InverterReadingRoutes {
+    var handler: NetworkClientHandler
+
+    init(handler: NetworkClientHandler) {
+        self.handler = handler
+    }
+
+    public func latest() async throws -> StoredInverterReading {
+        try await handler.get(path: "inverter_readings/latest")
+    }
+}
